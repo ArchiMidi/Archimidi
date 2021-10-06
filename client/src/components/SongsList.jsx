07810ba7e247fddc9d72ec1abee3579b898791ee
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-10-06 22:59:38 +0200

    improve search; add tags search

--- client/src/components/SongsList.jsx
+++ client/src/components/SongsList.jsx
@@ -11,86 +11,111 @@ import SongCard from "./SongCard";
 
 
 function SongsList(props) {
-
+  
   const [allSongs, setAllSongs] = useState([]);
   const [search, setSearch] = useState('');
-  const [searchFields, setSearchFields] = useState({ title: true, author: true })
-
+  const [searchFields, setSearchFields] = useState({ title: true, author: true, tags: false })
+  
   const getAllSongs = () => {
     return service
-      .findAllSongs()
-      .then(response => {
-        // console.log("response is: ", response);
-        setAllSongs(response)
-      })
-      .catch(err => console.log('Error while uploading the file: ', err));
+    .findAllSongs()
+    .then(response => {
+      // console.log("response is: ", response);
+      setAllSongs(response)
+    })
+    .catch(err => console.log('Error while uploading the file: ', err));
   }
-
-
-
+  
+  
+  
   //filter
-
+  
   useEffect(() => {
     getAllSongs()
   }, [])
-
-
+  
+  
   let words = search.split(' ')
-  let searchArg = (words.length === 1) ? words : words.join('.*')
-  let Regx1 = new RegExp(searchArg, 'i')
-  let searchArgRev = (words.length === 1) ? words : words.join('.*') + '|' + words.reverse().join(".*");
-  let Regx2 = new RegExp(searchArgRev, 'i')
-
-
-
+  
+  function regXCheck (search, word) {
+    const Regx1 = new RegExp(search, 'i');
+    return Regx1.test(word);
+  }
+  
   const filteredSongs = allSongs.filter(song => {
-
-    if (searchFields.title === true && searchFields.author === true) {
-      return ((Regx1.test(song.title) || Regx2.test(song.title)) || (Regx1.test(song.author) || Regx2.test(song.author)))
-
-    } else if (searchFields.title === false && searchFields.author === true) {
-      return Regx1.test(song.author) || Regx2.test(song.author)
-
-    } else if (searchFields.title === true && searchFields.author === false) {
-      return Regx1.test(song.title) || Regx2.test(song.title)
-
-    } else {
-      return allSongs
+    const title = song.title.split(' ')
+    const author = song.author.split(' ')
+    const tags = [...song.tags]
+    
+    for (let word of words){
+      if (searchFields.title === true){
+        for (let str of title){
+          if (regXCheck(word, str)) {
+            return true;
+          }
+        }
+      }
+      if (searchFields.author === true){
+        for (let str of author){
+          if (regXCheck(word, str)) {
+            return true;
+          }
+        }
+      }
+      if (searchFields.tags === true){
+      for (let str of tags){
+        if (regXCheck(word, str)) {
+          return true;
+        }
+      }
     }
-  })
-
-
+      return false;
+    }
+  });
+  
+  
   // const songsList = allSongs.map(song => <div key={song._id}><h1>{song.title}</h1><a href={song.songUrl} download={`${song.title}_${song.author}.midi`}>Download</a></div>)
-
+  
   return (<>
     <h1>List of Songs</h1>
     <div className="filter">
-      <label>Search: </label>
-      <input className='searchBar' type="search" name="search" value={search} onChange={e => setSearch(e.target.value)} />
+    <label>Search: </label>
+    <input className='searchBar' type="search" name="search" value={search} onChange={e => setSearch(e.target.value)} />
     </div>
     <label>
-      Search by Title
-      <input
-        name="title"
-        type="checkbox"
-        checked={searchFields.title}
-        onChange={e => setSearchFields({ ...searchFields, title: e.target.checked })} />
+    Search by Title
+    <input
+    name="title"
+    type="checkbox"
+    checked={searchFields.title}
+    onChange={e => setSearchFields({ ...searchFields, title: e.target.checked })} />
     </label>
     <br />
     <label>
-      Search by Author
-      <input
-        name="author"
-        type="checkbox"
-        checked={searchFields.author}
-        onChange={e => setSearchFields({ ...searchFields, author: e.target.checked })} />
+    Search by Author
+    <input
+    name="author"
+    type="checkbox"
+    checked={searchFields.author}
+    onChange={e => setSearchFields({ ...searchFields, author: e.target.checked })} />
     </label>
+    <br />
+    <label>
+    Search through the tags
+    <input
+    name="tags"
+    type="checkbox"
+    checked={searchFields.tags}
+    onChange={e => setSearchFields({ ...searchFields, tags: e.target.checked })} />
+    </label>
+    <br />
     {/* {songsList} */}
     <div className='songsListContainer'>
-      {filteredSongs.map(song => <SongCard className='songCard' key={song._id} {...song} />)}
+    {filteredSongs.map(song => <SongCard className='songCard' key={song._id} {...song} />)}
     </div>
-  </>
-  );
-}
-
-export default SongsList;
+    </>
+    );
+  }
+  
+  export default SongsList;
+  

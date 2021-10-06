3126e6fe16dddbec48e40b5601a7ad06532b6b0a
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-10-06 13:19:03 +0200

    depoyment mods

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require("dotenv/config");
 
 // ℹ️ Connects to the database
 require("./db");
-
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -30,29 +29,36 @@ app.use(
             mongoUrl: DB_URL
         })
     })
-)
-
-
-
-// 👇 Start handling routes here
-// Contrary to the views version, all routes are controlled from the routes/index.js
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
-
-// app.js
-
-//  Start handling routes here
-const index = require('./routes/index'); // <== already included
-app.use('/', index); // <== already included
-
-const songRouter = require('./routes/midiSong'); // <== has to be added
-app.use('/api', songRouter); // <== has to be added
-
-const auth = require('./routes/auth');
-app.use('/api/auth', auth)
-
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
-
+    )
+    
+    
+    
+    // 👇 Start handling routes here
+    // Contrary to the views version, all routes are controlled from the routes/index.js
+    const allRoutes = require("./routes");
+    app.use("/api", allRoutes);
+    
+    // app.js
+    
+    //  Start handling routes here
+    const index = require('./routes/index'); // <== already included
+    app.use('/', index); // <== already included
+    
+    const songRouter = require('./routes/midiSong'); // <== has to be added
+    app.use('/api', songRouter); // <== has to be added
+    
+    const auth = require('./routes/auth');
+    app.use('/api/auth', auth)
+    
+    const path = require('path');
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    
+    app.use((req, res) => {
+        // If no routes match, send them the React HTML.
+        res.sendFile(__dirname + "/client/build/index.html");
+    });
+    
+    // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+    require("./error-handling")(app);
+    
 module.exports = app;
